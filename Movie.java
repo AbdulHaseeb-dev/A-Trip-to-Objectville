6bbb6923d79d4c8b763b21dc0f44fa85c8feb6cd
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-02-14 10:25:21 +0500

    All exercises of CHP 2 completed

---
+++ Movie.java
@@ -0,0 +1,12 @@
+class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void platIt(){
+        System.out.println("Playing the movie");
+        System.out.println("Title:" + " " + title);
+        System.out.println("Genre:" + " " + genre);
+        System.out.println("Rating:" + " " + rating);
+    }
+}
